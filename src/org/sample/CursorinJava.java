272a0f6fa0b1b71fd8862e7920ac878fc4a85e53
Sakthi1605 <vjsakthi619@gmail.com>
2022-05-22 00:18:23 +0530

    added

---
+++ src/org/sample/CursorinJava.java
@@ -0,0 +1,103 @@
+package org.sample;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class CursorinJava {
+	
+	public static void main(String[] args) {
+		
+	//Enumeration	
+		
+		Vector<String> v = new Vector<>();
+		
+		v.add("A");
+		v.add("B");
+		v.add("c");
+		v.add("d");
+		v.add("E");
+		
+	System.out.println(v);
+		
+	Enumeration<String> elements = v.elements();	
+		
+		while (elements.hasMoreElements()) {
+			String string =  elements.nextElement();
+			System.out.println(string);
+			
+		}
+		
+		
+		//iterator
+		
+		Iterator<String> iterator = v.iterator();
+		
+		
+		while (iterator.hasNext()) {
+			String string =  iterator.next();
+		
+			if (string.equals("E")) {
+				iterator.remove();
+				
+			}
+		}
+		System.out.println(v);
+		
+		
+	
+	
+	
+	//list iterator
+	
+	
+	ListIterator<String> listIterator = v.listIterator();
+	
+	while (listIterator.hasNext()) {
+		String string =  listIterator.next();
+		System.out.println( string);
+         if (string.equals("A")) {
+        	 
+        	 listIterator.add("java");
+		}else if (string.equals("B")) {
+			 listIterator.remove();
+			
+		}else if (string.equals("d")) {
+			 listIterator.set("zebra");
+			
+		}
+		System.out.println(v);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
